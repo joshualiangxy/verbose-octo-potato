44e692177a23ae915303989d404870ec4b74fe50
joshualiangxy <joshualiang.xy@gmail.com>
2021-10-31 19:37:58 +0800

    Add logic for map

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 
 enum TAG { MAP_SEND, MAP_RECEIVE, REDUCE_SEND, REDUCE_RECEIVE, EXIT };
 
+int MASTER_RANK = 0;
 int NUM_MASTER = 1;
 int MAX_MAP_KEYS = 4 * 48;
 int MAX_REDUCE_KEYS = 26;
@@ -96,37 +97,16 @@ void master(
         }
 
         buffer[i] = i;
-        MPI_Isend(
-            &buffer[i],
-            1,
-            MPI_INT,
-            map_worker_rank,
-            MAP_SEND,
-            MPI_COMM_WORLD,
-            &send_requests[i]
-        );
-        MPI_Irecv(
-            map_results[i],
-            MAX_MAP_KEYS,
-            mpi_key_value_type,
-            map_worker_rank,
-            MAP_RECEIVE,
-            MPI_COMM_WORLD,
-            &receive_requests[i]
-        );
+        MPI_Isend(&buffer[i], 1, MPI_INT, map_worker_rank,
+                MAP_SEND, MPI_COMM_WORLD, &send_requests[i]);
+        MPI_Irecv(map_results[i], MAX_MAP_KEYS, mpi_key_value_type, map_worker_rank,
+                MAP_RECEIVE, MPI_COMM_WORLD, &receive_requests[i]);
     }
 
     for (i = 0; i < num_map_workers; i++) {
         map_worker_rank = NUM_MASTER + i;
-        MPI_Isend(
-            NULL,
-            0,
-            MPI_INT,
-            map_worker_rank,
-            EXIT,
-            MPI_COMM_WORLD,
-            &send_requests[i]
-        );
+        MPI_Isend(NULL, 0, MPI_INT, map_worker_rank,
+                EXIT, MPI_COMM_WORLD, &send_requests[i]);
     }
 
     while (completed++ < num_files) {
@@ -145,24 +125,10 @@ void master(
 
     for (i = 0; i < num_reduce_workers; i++) {
         reduce_worker_rank = NUM_MASTER + num_map_workers + i;
-        MPI_Isend(
-            NULL,
-            0,
-            mpi_key_value_type,
-            reduce_worker_rank,
-            EXIT,
-            MPI_COMM_WORLD,
-            &send_requests[i]
-        );
-        MPI_Irecv(
-            &reduce_results[i],
-            MAX_REDUCE_KEYS,
-            mpi_key_value_type,
-            reduce_worker_rank,
-            REDUCE_RECEIVE,
-            MPI_COMM_WORLD,
-            &receive_requests[i]
-        );
+        MPI_Isend(NULL, 0, mpi_key_value_type, reduce_worker_rank,
+            EXIT, MPI_COMM_WORLD, &send_requests[i]);
+        MPI_Irecv(&reduce_results[i], MAX_REDUCE_KEYS, mpi_key_value_type, reduce_worker_rank,
+            REDUCE_RECEIVE, MPI_COMM_WORLD, &receive_requests[i]);
     }
 
     for (i = 0; i < num_reduce_workers; i++) {
@@ -170,16 +136,81 @@ void master(
         MPI_Get_count(&status, mpi_key_value_type, &count);
         write_to_file(reduce_results[i], count, output_file);
     }
+
+    fclose(output_file);
+}
+
+void partition_results(
+    KeyValue* key_values,
+    int length,
+    int num_reduce_workers,
+    KeyValueMessage* send_buffer
+) {
+    int i;
+
+    for (i = 0; i < length; i++) {
+        KeyValue key_value = key_values[i];
+
+        send_buffer[i].val = key_value.val;
+        send_buffer[i].partition = partition(key_value.key, num_reduce_workers);
+        strcpy(send_buffer[i].key, key_value.key);
+    }
 }
 
 void map_worker(
-    int rank,
+    int map_reduce_task_num,
+    int num_reduce_workers,
     MapTaskOutput* map(char*),
     MPI_Datatype mpi_key_value_type
 ) {
     FILE* input_file;
+    int file_num;
+    long file_size;
+    char* file_contents;
+    MPI_Status status;
+    MPI_Request send_request;
+    KeyValueMessage* send_buffer;
+
     while (true) {
-        MPI_Status status;
+        char file_name[16];
+
+        MPI_Recv(&file_num, 1, MPI_INT, MASTER_RANK,
+                MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        if (status.MPI_TAG == EXIT) break;
+        if (status.MPI_TAG != MAP_SEND) {
+            printf("Wrong tag sent to map worker: %d\n", status.MPI_TAG);
+            continue;
+        }
+
+        snprintf(file_name, sizeof(char) * 16, "%d.txt", file_num);
+
+        input_file = fopen(file_name, "rb");
+
+        fseek(input_file, 0, SEEK_END);
+        file_size = ftell(input_file);
+        fseek(input_file, 0, SEEK_SET);
+
+        file_contents = (char*) malloc(file_size + 1);
+        fread(file_contents, file_size, input_file);
+
+        fclose(file_name);
+
+        file_contents[file_size] = 0;
+
+        MapTaskOutput results = map(file_contents);
+
+        send_buffer = (KeyValueMessage*) malloc(
+            sizeof(KeyValueMessage) * results.len
+        );
+
+        partition_results(results.kvs, results.len,
+                num_reduce_workers, send_buffer);
+
+        MPI_Wait(&send_request, MPI_IGNORE_STATUS);
+
+        MPI_Isend(send_buffer, results.len, mpi_key_value_type, MASTER_RANK,
+                MAP_RECEIVE, MPI_COMM_WORLD, &send_request);
     }
 }
 
@@ -265,7 +296,7 @@ int main(int argc, char** argv)
             rank,
             rank - NUM_MASTER
         );
-        map_worker(rank - 1, map, mpi_key_value_type);
+        map_worker(map_reduce_task_num, num_reduce_workers, map, mpi_key_value_type);
     } else {
         printf(
             "Rank (%d => %d): This is a reduce worker process\n",
